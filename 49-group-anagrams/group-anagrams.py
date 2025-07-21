65416a4e4d905b7c4e67c0668ea023a759da4355
Vaibhav krishna <vr6802383@gmail.com>
2025-07-21 19:51:44 +0530

    Time: 15 ms (96.27%) | Memory: 16 MB (93.46%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            # Sort the word to form the key
+            key = ''.join(sorted(word))
+            anagram_map[key].append(word)
+
+        return list(anagram_map.values())
