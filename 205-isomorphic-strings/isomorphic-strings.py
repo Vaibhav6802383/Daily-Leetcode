e0b4c5c917093e9de47051cfa0c91fd3c5c65686
Vaibhav krishna <vr6802383@gmail.com>
2025-07-21 19:47:18 +0530

    Time: 7 ms (87.74%) | Memory: 15.2 MB (8.64%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        
+        s_to_t = {}
+        t_to_s = {}
+
+        for ch_s, ch_t in zip(s, t):
+            if ch_s in s_to_t:
+                if s_to_t[ch_s] != ch_t:
+                    return False
+            else:
+                if ch_t in t_to_s:
+                    return False
+                s_to_t[ch_s] = ch_t
+                t_to_s[ch_t] = ch_s
+
+        return True
